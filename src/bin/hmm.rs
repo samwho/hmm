a0b6c1216cf641b1b224cddb31ad1c5ff77d5275
Sam Rose <samwho@lbak.co.uk>
2020-03-12 22:30:30 +0000

    More integration tests.

--- src/bin/hmm.rs
+++ src/bin/hmm.rs
@@ -69,11 +69,13 @@ fn compose_entry(editor: &str) -> Result<String> {
 
 #[cfg(test)]
 mod tests {
-    use assert_cmd::Command;
+    use assert_cmd::{assert::Assert, Command};
+    use chrono::{DateTime, Utc};
     use hmm::{config::Config, entry::Entry, Result};
     use std::convert::TryInto;
     use std::fs::File;
     use std::io::{BufRead, BufReader, Seek, SeekFrom, Write};
+    use std::path::PathBuf;
     use tempfile::NamedTempFile;
     use test_case::test_case;
 
@@ -86,36 +88,72 @@ mod tests {
         Ok(f)
     }
 
+    fn run_with_config(config: &Config, args: Vec<&str>) -> Assert {
+        let config_path = mkconfig(&config).unwrap();
+        Command::cargo_bin("hmm")
+            .unwrap()
+            .arg("--config")
+            .arg(config_path.path())
+            .args(args)
+            .assert()
+    }
+
+    fn new_tempfile_path() -> PathBuf {
+        NamedTempFile::new().unwrap().keep().unwrap().1
+    }
+
     #[test_case(vec!["hello world"]      => "hello world"   ; "single argument, single line entry")]
     #[test_case(vec!["hello", "world"]   => "hello world"   ; "multiple argument, single line entry")]
     #[test_case(vec!["hello\nworld"]     => "hello\nworld"  ; "single argument, multiple line entry")]
     #[test_case(vec!["hello\n", "world"] => "hello\n world" ; "multiple argument, multiple line entry")]
     fn test_hmm_single_invocation(args: Vec<&str>) -> String {
-        let t = NamedTempFile::new().unwrap();
-        let path = t.keep().unwrap().1;
-
         let config = Config {
-            path: Some(path),
+            path: Some(new_tempfile_path()),
             ..Default::default()
         };
-        let config_path = mkconfig(&config).unwrap();
 
-        let assert = Command::cargo_bin("hmm")
-            .unwrap()
-            .arg("--config")
-            .arg(config_path.path())
-            .args(args)
-            .assert();
+        let assert = run_with_config(&config, args);
         assert.success();
 
         let mut buf = String::new();
-
         BufReader::new(File::open(config.path().unwrap()).unwrap())
             .read_line(&mut buf)
             .unwrap();
 
         let entry: Entry = buf.as_str().try_into().unwrap();
-
         entry.message().to_owned()
+    }
+
+    #[test_case(vec!["1", "2"]           => vec!["1", "2"]           ; "two invocations")]
+    #[test_case(vec!["1", "2", "3"]      => vec!["1", "2", "3"]      ; "three invocations")]
+    #[test_case(vec!["1", "2", "3", "4"] => vec!["1", "2", "3", "4"] ; "four invocations")]
+    fn test_hmm_multiple_invocations(messages: Vec<&str>) -> Vec<String> {
+        let config = Config {
+            path: Some(new_tempfile_path()),
+            ..Default::default()
+        };
+
+        for message in &messages {
+            let assert = run_with_config(&config, vec![message]);
+            assert.success();
+        }
+
+        // Start with the earliest possible date, as we're going to compare the dates we find
+        // in the resulting file with this value to make sure they always increase.
+        let mut date: DateTime<Utc> = DateTime::parse_from_rfc3339("1970-01-01T00:00:00+00:00")
+            .unwrap()
+            .into();
+
+        let mut messages: Vec<String> = Vec::with_capacity(messages.len());
+        let r = BufReader::new(File::open(config.path().unwrap()).unwrap());
+        for line in r.lines() {
+            let entry: Entry = line.unwrap().as_str().try_into().unwrap();
+            messages.push(entry.message().to_owned());
+
+            assert_eq!(true, &date <= entry.datetime());
+            date = entry.datetime().to_owned();
+        }
+
+        messages
     }
 }
